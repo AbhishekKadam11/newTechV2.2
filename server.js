0016fcd1b095a2268f778de59f025ebfb130783c
Abhishek Kadam <abhishek.kadam007@gmail.com>
2018-02-25 20:47:07 +0530

    dynamic port selection on server file.

--- server.js
+++ server.js
@@ -5,4 +5,6 @@ var app = express();
 app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
-app.listen(process.env.PORT || 3000);
+app.listen(config.port, function () {
+  console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
+});
