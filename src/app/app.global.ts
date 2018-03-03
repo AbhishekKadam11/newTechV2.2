d263a7d27a2815032eabc14e2912261bfb222a57
Abhishek Kadam <abhishek.kadam007@gmail.com>
2018-03-03 22:35:39 +0530

    -fix server url path for images.

--- src/app/app.global.ts
+++ src/app/app.global.ts
@@ -3,7 +3,8 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class GlobalShared {
   // console.log(window.location.href);
-  urlpath: string = 'http://localhost:8080/uploads/';
+  urlpath: string = ((window.location.href).substr(0,5 ) === 'https') ? 'http://localhost:8080/uploads/' :
+    'https://newtechserver.herokuapp.com/uploads' ;
   serverpath: string = ((window.location.href).substr(0,5 ) === 'https') ? 'http://localhost:8080/api/' :
     'https://newtechserver.herokuapp.com/api/';
 }
