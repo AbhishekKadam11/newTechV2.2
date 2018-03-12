89f1747b7f5fc883a3bc84dc03c1ec386787e528
Abhishek Kadam <abhishek.kadam007@gmail.com>
2018-03-12 22:12:34 +0530

    -working on dynamic image path change.

--- src/app/app.global.ts
+++ src/app/app.global.ts
@@ -3,8 +3,10 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class GlobalShared {
   // console.log(window.location.href);
-  urlpath: string = ((window.location.href).substr(0,5 ) === 'https') ? 'https://newtechserver.herokuapp.com/uploads/' :
-    'http://localhost:8080/uploads/' ;
-  serverpath: string = ((window.location.href).substr(0,5 ) === 'https') ? 'https://newtechserver.herokuapp.com/api/' :
-    'http://localhost:8080/api/';
+  // urlpath: string = ((window.location.href).substr(0,5 ) === 'https') ? 'https://newtechserver.herokuapp.com/uploads/' :
+  //   'http://localhost:8080/uploads/' ;
+  // serverpath: string = ((window.location.href).substr(0,5 ) === 'https') ? 'https://newtechserver.herokuapp.com/api/' :
+  //   'http://localhost:8080/api/';
+  urlpath: string = 'https://newtechserver.herokuapp.com/uploads/';
+  serverpath: string = 'https://newtechserver.herokuapp.com/api/';
 }
